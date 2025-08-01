ab8424a604431266e13196fbdc0d53c21ee69b18
Vaishnavpatil5124 <patilvaishnav30@gmail.com>
2025-08-01 22:13:08 +0530

    modified:   fundamentals/logical-operators.js

---
+++ fundamentals/logical-operators.js
@@ -0,0 +1,15 @@
+const physics=90;
+const maths=45;
+const sst=90;
+const chem=85;
+
+if(physics>85 && chemistry > 80){
+    console.log("you are eligible for iit")
+}
+else{
+    console.log("you are not eligible for iit")
+}
+// && operator
+// || operator
+// NOT
+// nullish coalescing
