2b677e36469af54ef335210ed9704e322b3e66bf
Vaishnavpatil5124 <patilvaishnav30@gmail.com>
2025-08-04 02:50:19 +0530

    new file:   fundamentals/logical_operator2.js

---
+++ fundamentals/logical_operator2.js
@@ -0,0 +1,8 @@
+const firstname="vaishnav"
+const nickname="";
+console.log(firstname || nickname);
+
+// checks if there is value in first name and print the variable having value
+let a=12;
+let b;
+console.log(a+(b||0))
