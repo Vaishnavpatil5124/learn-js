84ae044a7f35e601156ec38ffa6a51c784a30e8d
Vaishnavpatil5124 <patilvaishnav30@gmail.com>
2025-08-05 22:15:13 +0530

    modified:   fundamentals/logical_operator2.js 	new file:   fundamentals/nullish.js

--- fundamentals/logical_operator2.js
+++ fundamentals/logical_operator2.js
@@ -1,8 +1,17 @@
-const firstname="vaishnav"
+const firstname=null;
 const nickname="";
-console.log(firstname || nickname);
+// console.log(firstname || nickname);
 
-// checks if there is value in first name and print the variable having value
-let a=12;
-let b;
-console.log(a+(b||0))
+// // checks if there is value in first name and print the variable having value
+// let a=12;
+// let b;
+// console.log(a+(b||0))
+
+const username=firstname && nickname;
+console.log(username);
+
+
+/** or || checks for first truthee value if it doesnt find it will give last falsey value
+ * 
+ * && search for first falsey value and it will return last truthee value
+ */
