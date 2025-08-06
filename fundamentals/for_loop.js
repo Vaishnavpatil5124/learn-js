e575b7a16e3a5c6b97d715e6f91b68c529c9cb7e
Vaishnavpatil5124 <patilvaishnav30@gmail.com>
2025-08-06 23:56:52 +0530

    new file:   fundamentals/for_loop.js

---
+++ fundamentals/for_loop.js
@@ -0,0 +1,21 @@
+// for(let i=0;i<=10;i++){  // here i is a local scope
+//     console.log(`hello world ${i}`)
+// }
+
+const naam="vaishnav";
+let len=naam.length;
+
+for(i=0;i<len;i++){
+    console.log(naam[i])
+}
+
+/* output=
+v
+a
+i
+s
+h
+n
+a
+v
+*/
