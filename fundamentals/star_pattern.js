db54c8275939ed1357db373954d41c450dc4fb59
Vaishnavpatil5124 <patilvaishnav30@gmail.com>
2025-08-10 23:21:13 +0530

    modified:   fundamentals/star_pattern.js 	new file:   fundamentals/star_reverse.js

--- fundamentals/star_pattern.js
+++ fundamentals/star_pattern.js
@@ -1,9 +1,19 @@
-const pattern=require("readline-sync")
-const rows=Number(pattern.question("eneter number of rows:"));
+
 
 const symbol="*"
-for(let i=0;i<=rows;i++){
+for(let i=0;i<=5;i++){
     
     console.log(symbol.repeat(i));   
     
 }
+
+
+// const pattern=require("readline-sync")
+// const rows=Number(pattern.question("eneter number of rows:"));
+
+// const symbol="*"
+// for(let i=0;i<=rows;i++){
+    
+//     console.log(symbol.repeat(i));   
+    
+// }
---
+++ fundamentals/star_reverse.js
@@ -0,0 +1,4 @@
+const symbol="*";
+for(let i=5;i>0;i--){
+    console.log(symbol.repeat(i))
+}
