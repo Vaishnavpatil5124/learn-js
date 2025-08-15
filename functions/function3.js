fcb27bd8780f84ffa3d84bff285de08e2f4c991f
Vaishnavpatil5124 <patilvaishnav30@gmail.com>
2025-08-15 19:51:27 +0530

    modified:   functions/function3.js

--- functions/function3.js
+++ functions/function3.js
@@ -1,5 +1,8 @@
-let calculateSum = (x, y) => {
-    return x + y;
-};
+// let calculateSum = (x, y) => {
+//     return x + y;
+// };
 
-console.log(calculateSum(12, 4)); // Output: 16
+// console.log(calculateSum(12, 4)); // Output: 16
+
+const calculateSum=(x,y)=>x>y? x-y:y-x;//ternanry operator
+console.log(calculateSum(15,17));
