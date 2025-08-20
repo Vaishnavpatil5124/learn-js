0ebeec93d95cd98fd63bff5483798f0843597c6a
Vaishnavpatil5124 <patilvaishnav30@gmail.com>
2025-08-20 23:33:34 +0530

    modified:   string_methods/substring.js

--- string_methods/substring.js
+++ string_methods/substring.js
@@ -2,3 +2,13 @@ const fame="my name is vaishnav";
 const sub=fame.substring(0,10); // index based
 
 console.log(sub);
+
+const username = "prakashnarsingrao sakari";
+const maxLength = 10;
+
+function getDisplayName(username, maxLength) {
+    return username.length > maxLength ? username.substring(0, maxLength) + "..." : username;
+}
+
+const displayName = getDisplayName(username, maxLength);
+console.log(displayName); // Output: "prakashnar..."
