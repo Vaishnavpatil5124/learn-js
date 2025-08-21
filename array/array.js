7897398e4385c48d7d6f7d60e18b44c071dfa156
Vaishnavpatil5124 <patilvaishnav30@gmail.com>
2025-08-21 23:57:05 +0530

    array basics 	new file:   array/array.js

---
+++ array/array.js
@@ -0,0 +1,18 @@
+/** Non primituve data type */
+
+// let students=["vaishnav","rahul","raj",1,2,3,["html","css"],{schoolname:"dyp"}];
+// console.log(students);
+
+let naam=["raj","rahul","ram","shyaam"]
+// console.log(naam[0]);
+// console.log(naam[1]);
+// console.log(naam[2]);
+// console.log(naam[3]);
+
+// for(let i=0;i<naam.length;i++){
+//     console.log(naam[i]);
+// }
+
+for(let name of naam){
+    console.log(name);
+}
