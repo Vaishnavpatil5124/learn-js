f91b17c9691c7b8fdc42532ac34f8bb74d216890
Vaishnavpatil5124 <patilvaishnav30@gmail.com>
2025-08-25 23:58:14 +0530

    split_and_join 	new file:   array/split_and_join.js

---
+++ array/split_and_join.js
@@ -0,0 +1,12 @@
+/**split and join */
+
+
+// const arr=[1,2,3,4,5]
+// console.log(arr.reverse());
+
+const input="madam";
+const arrofchar=input.split("");
+console.log(arrofchar);
+
+input.reverse().join("");
+console.log(input);
