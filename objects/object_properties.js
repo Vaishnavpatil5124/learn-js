dac34bcaf6c0589fb5f659caac1fb6b1905f3e51
Vaishnavpatil5124 <patilvaishnav30@gmail.com>
2025-08-28 23:57:41 +0530

    modified:   objects/object_properties.js

--- objects/object_properties.js
+++ objects/object_properties.js
@@ -1,9 +1,11 @@
 /**object---> key:value pairs */
 
 const obj={
-    name:"vaishnav",
+    name:"vaishnav",  // keys are always in string
     age:99,
     job:"idk",
     course:["html","css","js"]
 }; //object literal
-console.log( obj);
+console.log(obj);
+console.log(obj.age);
+console.log(obj["name"]);
