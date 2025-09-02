c26de89f26251f205609ae53d27db0da324da98d
Vaishnavpatil5124 <patilvaishnav30@gmail.com>
2025-09-02 23:49:46 +0530

    modified:   objects/optional_chaining.js

---
+++ objects/optional_chaining.js
@@ -0,0 +1,11 @@
+const user={
+    name:"vaishnav",
+    age:20,
+    address:{
+        street:"idk",
+        city:"pune"
+    },
+    likes:"eat code repeat"
+}
+
+console.log(user.hobbies); // undefined
